4645db299b5e9f110d75c0918319d577f6db5502
Marcelo Salada <97004501+MarceloSalada@users.noreply.github.com>
2026-04-18 09:51:53 -0300

    chore: add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './lib/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#020617',
+        foreground: '#e2e8f0',
+      },
+    },
+  },
+  plugins: [],
+};
